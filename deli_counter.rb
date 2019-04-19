8fe4b92744ee1dec3f8467ec45b3c9e2a80a327a
Oscar Shoudt <shoudto@gmail.com>
2019-04-19 19:11:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,19 @@
+# start of code
+
 def line(katz_deli)
-  puts "The line is currently empty."
+    if katz_deli == 0
+      puts "The line is currently empty."
+    elsif
+  end 
 end 
+
+# the next method 
 
 def take_a_number
   
 end 
+
+# the last method 
 
 def now_serving 
   
