18c533faff391f1387f112cb9e4b1ae690001a77
Oscar Shoudt <shoudto@gmail.com>
2019-04-19 19:22:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+# start of code
+
+def line(katz_deli)
+    if katz_deli == 0
+      puts "The line is currently empty."
+    elsif 
+  end 
+end 
+
+# the next method 
+
+def take_a_number
+  
+end 
+
+# the last method 
+
+def now_serving 
+  
+end 
